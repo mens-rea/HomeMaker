093e13d83b274c0dcf4c3cfcee403c9852e8eb09
mens-rea <juan.miguel.g.alvarez@gmail.com>
2022-01-27 07:52:18 +0800

    add home page

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>HomeMaker</h1>
+  <p>Household management should be fun...</p>
+</template>
+
+<style scoped>
+  /* Place your custom css here */
+</style>
